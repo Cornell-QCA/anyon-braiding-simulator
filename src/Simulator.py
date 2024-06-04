8ac65211b6357ad2a85341ea55250652b720e563
Haadi Khan <haadikhan05@gmail.com>
2024-06-04 13:09:56 -0400

    Simulator done for now (waiting on other classes)
    Also fixed a typo in main.py

--- src/Simulator.py
+++ src/Simulator.py
@@ -7,3 +7,24 @@ class Simulator:
        self._anyons = []
        self._model = None
 
+    def update_anyons(self, is_increasing: bool, anyons) -> None:
+        '''
+        Update the anyons stored in memory.
+
+        is_increasing: bool - True if the anyons are being added, False if the anyons are being removed.
+        anyons: list - List of anyons to add or remove.
+        '''
+        if is_increasing:
+            self._anyons.extend(anyons)
+        else:
+            self._anyons = [anyon for anyon in self._anyons if anyon not in anyons]
+
+    def set_model(self, model) -> None:
+        '''
+        Set the model for the simulation.
+
+        model: Model - The model to set.
+        '''
+        self._model = model
+
+    # waiting on other classes to be implemented
