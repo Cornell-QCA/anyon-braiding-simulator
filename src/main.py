f27f296b53ef0d87423e2832ac2d074a642baf4d
Haadi Khan <haadikhan05@gmail.com>
2024-06-04 12:52:43 -0400

    Updated Shell to use cmd (more like proper repl)

--- src/main.py
+++ src/main.py
@@ -1,52 +1,114 @@
-from functools import partial
+import cmd
+from Simulator import Simulator
+
+sim = Simulator()
 
 def anyon(*args):
-    print("anyon")
+    '''
+    Handle the anyon command. This command adds an anyon to the simulation.
+    '''
+    if len(args) < 3:
+        print("Error: Not enough arguments")
+        return
+
+    name = args[0]
+
+    try:
+        topological_charge = float(args[1])
+    except ValueError:
+        print("Error: topological charge must be a number")
+        return
+
+    try:
+        position = tuple(map(float, args[2].replace("{", "").replace("}", "").split(",")))
+    except ValueError:
+        print("Error: position must be formatted as {x, y} where x and y are numbers")
+        return
+
+    # waiting on Anyon class
+    # anyon = Anyon(name, topological_charge, position)
+    # sim.update_anyons(True, [anyon])
+
+    print(f"Created anyon {name} with TC {topological_charge} at position {position}")
 
 def model(*args):
+    '''
+    Handle the model command. This command sets the model for the simulation.
+    '''
     print("model")
 
 def fusion(*args):
+    '''
+    Handle the fusion command. This command fuses anyons together.
+    '''
     print("fusion")
 
 def braid(*args):
+    '''
+    Handle the braid command. This command braids anyons together.
+    '''
     print("braid")
 
+class SimulatorShell(cmd.Cmd):
+
+    last_command = ""
+
+    command_options = {
+        "anyon": "anyon <name> <topological charge> <{x,y} coords>",
+        "model": "model <Ising or Fibonacci>",
+        "fusion": "fusion anyon_name_1 anyon_name_2 ...",
+        "braid": "braid anyon_name_1 anyon_name_2 ...",
+    }
+
+    def do_shell(self, arg):
+        "Run a shell command"
+        print("running shell command:", arg)
+        import subprocess
+        subprocess.run(arg, shell=True)
+
+    def do_anyon(self, arg):
+        "Add an anyon to the simulation"
+        args = arg.split(' ')
+        if args[0] == "help" or args[0] == "-h":
+            print(command_options["anyon"])
+        else:
+            anyon(*args)
+
+
+    def do_model(self, arg):
+        "Set the model for the simulation"
+        args = arg.split(' ')
+        model(*args)
+        if args[0] == "help" or args[0] == "-h":
+            print(command_options["model"])
+        else:
+            model(*args)
+
+
+    def do_fusion(self, arg):
+        "Fuse anyons together"
+        args = arg.split(' ')
+        if args[0] == "help" or args[0] == "-h":
+            print(command_options["fusion"])
+        else:
+            fusion(*args)
+
+    def do_braid(self, arg):
+        "Braid anyons together"
+        args = arg.split(' ')
+        if args[0] == "help" or args[0] == "-h":
+            print(command_options["braid"])
+        else:
+            braid(*args)
+
+    def do_exit(self, arg):
+        "Exit the simulator"
+        return True
+
+    def do_help(self, arg):
+        "Print help"
+        print("Commands: anyon, model, fusion, braid, exit, help")
 
 if __name__ == "__main__":
-    while True:
-        user_input = input("> ")
-
-        args = user_input.split(' ')
-        command = args[0]
-
-        command_options = {
-            "anyon": "anyon <name> <topological charge> <{x,y} coords>",
-            "model": "model <Ising or Fibonacci>",
-            "fusion": "fusion anyon_name_1 anyon_name_2 ...",
-            "braid": "braid anyon_name_1 anyon_name_2 ...",
-        }
-
-        command_functions = {
-            "anyon": anyon,
-            "model": model,
-            "fusion": fusion,
-            "braid": braid,
-        }
-
-        if command == "exit":
-            break
-        elif command == "help":
-            print("Commands: anyon, model, fusion, braid, exit, help")
-
-        for cmd in command_options.keys():
-            if command == cmd:
-                if len(args) == 1:
-                    print("Add the -h flag for help with this command")
-                elif args[1] == "-h" or args[1] == "--help":
-                    print(command_options[cmd])
-                else:
-                    command_functions[cmd](*args[1:])
-
-        if command not in command_options.keys() and command not in ["exit", "help"]:
-            print("Command not found")
+    shell = SimulatorShell()
+    shell.cmdloop()
