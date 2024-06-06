e2b64c622a8dd24d0fa1abcd6db5cfd395a355ca
Haadi Khan <haadikhan05@gmail.com>
2024-06-06 01:46:49 -0400

    Corresponding simulator updates for new classes

--- src/Simulator.py
+++ src/Simulator.py
@@ -1,30 +1,41 @@
 class Simulator:
     def __init__(self):
-       '''
-       Creates a the simulator. In order for the simulator to be function, the
-       user must first initialize the model and anyons.
-       '''
-       self._anyons = []
-       self._model = None
+        """
+        Creates a the simulator. In order for the simulator to be function, the
+        user must first initialize the model and anyons.
+        """
+        self._anyons = []
+        self._model = None
 
     def update_anyons(self, is_increasing: bool, anyons) -> None:
-        '''
+        """
         Update the anyons stored in memory.
 
-        is_increasing: bool - True if the anyons are being added, False if the anyons are being removed.
+        is_increasing: bool - True if the anyons are being added, False if the
+        anyons are being removed.
         anyons: list - List of anyons to add or remove.
-        '''
+        """
         if is_increasing:
             self._anyons.extend(anyons)
         else:
             self._anyons = [anyon for anyon in self._anyons if anyon not in anyons]
 
     def set_model(self, model) -> None:
-        '''
-        Set the model for the simulation.
-
-        model: Model - The model to set.
-        '''
+        """
+        Set the model for the simulator.
+        """
         self._model = model
+
+    def list_anyons(self) -> list:
+        """
+        List the anyons currently in the simulator.
+        """
+        return self._anyons
+
+    def contains_anyon(self, anyon_name: str) -> bool:
+        """
+        Check if the anyon is in the simulator.
+        """
+        return anyon_name in [anyon.name for anyon in self._anyons]
 
     # waiting on other classes to be implemented
