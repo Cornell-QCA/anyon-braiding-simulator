c6558b562f1aa4dfdf31f2e6ce8fe1d8e5dbddd3
Austin4705 <35353582+Austin4705@users.noreply.github.com>
2024-06-11 00:50:18 -0400

    Implement methods for State

--- src/fusion/state.rs
+++ src/fusion/state.rs
@@ -24,18 +24,45 @@ impl State {
 
     /// Add an anyon to the state
     fn add_anyon(&mut self, anyon: Anyon) -> PyResult<(bool)> {
-        Ok((true))
+
+        self.anyons.push(anyon);
+        Ok(true)
+    }
+
+    /// Verify the operation
+    fn verify_operation(&self, time:u32, operation: FusionPair) -> bool {
+        let mut fusible_anyons: Vec<bool>;
+        for i in 1..self.anyons.len(){
+           fusible_anyons.push(true)
+        } 
+        for (t, op) in self.operations{
+            
+            fusible_anyons[op.anyon_2()] = false;
+            if t == time{
+                fusible_anyons[op.anyon_1()] = false;
+            }
+        }
+        for i in operation.anyon_1()+1..operation.anyon_2()-1{
+            if fusible_anyons[i]{
+                return false
+            }
+    
+        }
+        true
+
     }
 
     /// Add an operation to the state
     fn add_operation(&mut self, time: u32, operation: FusionPair) -> PyResult<(bool)> {
-        Ok((true))
+        let result  = Self::verify_operation(self, time, operation);
+        if !result{
+            return Ok(false);
+        }
+        self.operations.push((time, operation));
+
+        Ok(true)
     }
 
-    /// Verify the operation
-    #[staticmethod]
-    fn verify_operation() -> bool {
-        true
-    }
+
 
 }
