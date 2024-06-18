b352842cf64c0d33c64c08542c8c707e9709b6e6
Ryan Byrne <rtb88@cornell.edu>
2024-06-18 18:26:47 -0400

    Add 2D anyon cases & organize into test classes

--- python/tests/test_main.py
+++ python/tests/test_main.py
@@ -19,86 +19,120 @@ def exec(cmds: list[str]):
     finally:
         os.chdir('../..')
 
+class TestInit:
+    @pytest.mark.main
+    def test_model_ising(self):
+        cmds = ['ising', 'exit']
+        exec(cmds)
 
-@pytest.mark.main
-def test_model_ising():
-    cmds = ['ising', 'exit']
-    exec(cmds)
+    @pytest.mark.main
+    def test_model_fibonacci(self):
+        cmds = ['fibonacci', 'exit']
+        exec(cmds)
 
-@pytest.mark.main
-def test_model_fibonacci():
-    cmds = ['fibonacci', 'exit']
-    exec(cmds)
+    @pytest.mark.main
+    def test_one_anyon_vac(self):
+        cmds = ['ising', 'anyon1 vac', 'done', 'exit']
+        exec(cmds)
 
-@pytest.mark.main
-def test_model_invalid():
-    cmds = ['derp', 'exit']
-    exec(cmds)
+    @pytest.mark.main
+    def test_one_anyon_sigma(self):
+        cmds = ['ising', 'anyon1 sigma', 'done', 'exit']
+        exec(cmds)
 
-@pytest.mark.main
-def test_model_post_init():
-    cmds = ['ising', 'anyon1 psi', 'done', 'model fibonacci', 'exit']
-    exec(cmds)
+    @pytest.mark.main
+    def test_one_anyon_psi(self):
+        cmds = ['ising', 'anyon1 psi', 'done', 'exit']
+        exec(cmds)
 
-@pytest.mark.main
-def test_anyon_post_init():
-    cmds = ['ising', 'anyon1 psi', 'done', 'anyon anyon2 psi', 'exit']
-    exec(cmds)
+    @pytest.mark.main
+    def test_one_anyon_2D(self):
+        cmds = ['ising', 'anyon1 psi {1,2}', 'done', 'exit']
+        exec(cmds)
 
-@pytest.mark.main
-def test_one_anyon_vac():
-    cmds = ['ising', 'anyon1 vac', 'done', 'exit']
-    exec(cmds)
+    @pytest.mark.main
+    def test_two_anyons(self):
+        cmds = ['ising', 'anyon1 psi', 'anyon2 vac', 'done', 'exit']
+        exec(cmds)
 
-@pytest.mark.main
-def test_one_anyon_sigma():
-    cmds = ['ising', 'anyon1 sigma', 'done', 'exit']
-    exec(cmds)
+    @pytest.mark.main
+    def test_three_anyons(self):
+        cmds = ['ising', 'anyon1 vac', 'anyon2 sigma', 'anyon3 sigma', 'done', 'exit']
+        exec(cmds)
 
-@pytest.mark.main
-def test_one_anyon_psi():
-    cmds = ['ising', 'anyon1 psi', 'done', 'exit']
-    exec(cmds)
+    @pytest.mark.main
+    def test_three_anyons_2D(self):
+        cmds = ['ising', 'anyon1 psi {-1,-1}', 'anyon2 vac {2,-1}', 'anyon3 vac {0,0}', 'done', 'exit']
+        exec(cmds)
 
-@pytest.mark.main
-def test_two_anyons():
-    cmds = ['ising', 'anyon1 psi', 'anyon2 vac', 'done', 'exit']
-    exec(cmds)
+    @pytest.mark.main
+    def test_three_anyons_2D_overlapping(self):
+        cmds = ['ising', 'anyon1 psi {-1,-1}', 'anyon2 vac {2,-1}', 'anyon3 vac {2,-1}', 'done', 'exit']
+        exec(cmds)
 
-@pytest.mark.main
-def test_three_anyons():
-    cmds = ['ising', 'anyon1 vac', 'anyon2 sigma', 'anyon3 sigma', 'done', 'exit']
-    exec(cmds)
+    @pytest.mark.main
+    def test_four_anyons(self):
+        cmds = ['ising', 'anyon1 psi', 'anyon2 psi', 'anyon3 psi', 'anyon4 psi', 'done', 'exit']
+        exec(cmds)
 
-@pytest.mark.main
-def test_four_anyons():
-    cmds = ['ising', 'anyon1 psi', 'anyon2 psi', 'anyon3 psi', 'anyon4 psi', 'done', 'exit']
-    exec(cmds)
+class TestBraidAndFuse:
+    @pytest.mark.main
+    def test_braid_and_print(self):
+        cmds = ['ising', 'anyon1 psi', 'anyon2 sigma', 'anyon3 psi', 'done', 'braid swap anyon1 anyon2', 'braid print', 'exit']
+        exec(cmds)
 
-@pytest.mark.main
-def test_list_one_anyon():
-    cmds = ['ising', 'anyon1 psi', 'done', 'list', 'exit']
-    exec(cmds)
+    @pytest.mark.main
+    def test_braid_and_print_2D(self):
+        cmds = ['ising', 'anyon1 psi {8,-4}', 'anyon2 sigma {5,5}', 'anyon3 psi {1,-1}', 'done', 'braid swap anyon1 anyon2', 'braid print', 'exit']
+        exec(cmds)
+    
+    @pytest.mark.main
+    def test_fusion(self):
+        cmds = ['ising', 'anyon1 psi', 'anyon2 sigma', 'done', 'fusion anyon1 anyon2', 'exit']
+        exec(cmds)
 
-@pytest.mark.main
-def test_list_three_anyons():
-    cmds = ['ising', 'anyon1 psi', 'anyon2 psi', 'anyon3 vac', 'done', 'list', 'exit']
-    exec(cmds)
+    @pytest.mark.main
+    def test_fusion_2D(self):
+        cmds = ['ising', 'anyon1 psi {0,1}', 'anyon2 sigma {-1,500}', 'done', 'fusion anyon1 anyon2', 'exit']
+        exec(cmds)
 
-@pytest.mark.main
-def test_fusion_command():
-    cmds = ['ising', 'anyon1 psi', 'anyon2 sigma', 'done', 'fusion anyon1 anyon2', 'exit']
-    exec(cmds)
+class TestListAndHelp:
+    @pytest.mark.main
+    def test_list_one_anyon(self):
+        cmds = ['ising', 'anyon1 psi', 'done', 'list', 'exit']
+        exec(cmds)
 
-@pytest.mark.main
-def test_braid_and_print():
-    cmds = ['ising', 'anyon1 psi', 'anyon2 sigma', 'anyon3 psi', 'done', 'braid swap anyon1 anyon2', 'braid print', 'exit']
-    exec(cmds)
+    @pytest.mark.main
+    def test_list_three_anyons(self):
+        cmds = ['ising', 'anyon1 psi', 'anyon2 psi', 'anyon3 vac', 'done', 'list', 'exit']
+        exec(cmds)
 
-@pytest.mark.main
-def test_help_command():
-    cmds = ['ising', 'anyon1 psi', 'done', 'help', 'exit']
-    exec(cmds)
+    @pytest.mark.main
+    def test_list_three_anyons_2D(self):
+        cmds = ['ising', 'anyon1 psi {9,17}', 'anyon2 psi {4,-106}', 'anyon3 vac {-1,4}', 'done', 'list', 'exit']
+        exec(cmds)
+
+    @pytest.mark.main
+    def test_help_command(self):
+        cmds = ['ising', 'anyon1 psi', 'done', 'help', 'exit']
+        exec(cmds)
+
+class TestInvalidCommands:
+    @pytest.mark.main
+    def test_model_invalid(self):
+        cmds = ['derp', 'exit']
+        exec(cmds)
+
+    @pytest.mark.main
+    def test_model_post_init(self):
+        cmds = ['ising', 'anyon1 psi', 'done', 'model fibonacci', 'exit']
+        exec(cmds)
+
+    @pytest.mark.main
+    def test_anyon_post_init(self):
+        cmds = ['ising', 'anyon1 psi', 'done', 'anyon anyon2 psi', 'exit']
+        exec(cmds)
+
 
 # @pytest.mark.main
 # def test_shell():
