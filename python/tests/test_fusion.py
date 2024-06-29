81c35d85fbb67f43715ef06e995e6de8dee8ae79
Rishi Gujjar <rishigujjar763@gmail.com>
2024-06-29 15:33:25 -0400

    Implements fusion methods for Fibonacci Model

--- python/tests/test_fusion.py
+++ python/tests/test_fusion.py
@@ -1,32 +1,39 @@
 import pytest
-from anyon_braiding_simulator import Anyon, AnyonModel, Fusion, FusionPair, IsingTopoCharge, State, TopoCharge
+from anyon_braiding_simulator import Anyon, AnyonModel, Fusion, FusionPair, IsingTopoCharge, FibonacciTopoCharge, State, TopoCharge
 
 
 @pytest.fixture
-def state() -> State:
+def ising_state() -> State:
     state = State()
     for i in range(6):
         state.add_anyon(Anyon(f'{i}', TopoCharge.from_ising(IsingTopoCharge.Sigma), (0, 0)))
     return state
 
+@pytest.fixture
+def fibo_state() -> State:
+    state = State()
+    state.set_anyon_model(AnyonModel.Fibonacci)
+    for i in range(6):
+        state.add_anyon(Anyon(f'{i}', TopoCharge.from_fibonacci(FibonacciTopoCharge.Tau), (0,0)))
+    return state
 
 @pytest.mark.fusion
-def test_str_1(state):
-    state.add_operation(1, FusionPair(0, 1))
-    state.add_operation(1, FusionPair(2, 3))
-    state.add_operation(1, FusionPair(4, 5))
-    state.add_operation(2, FusionPair(2, 4))
-    state.add_operation(3, FusionPair(0, 2))
+def test_str_1(ising_state):
+    ising_state.add_operation(1, FusionPair(0, 1))
+    ising_state.add_operation(1, FusionPair(2, 3))
+    ising_state.add_operation(1, FusionPair(4, 5))
+    ising_state.add_operation(2, FusionPair(2, 4))
+    ising_state.add_operation(3, FusionPair(0, 2))
 
-    fusion = Fusion(state)
+    fusion = Fusion(ising_state)
     expected = '0 1 2 3 4 5 \n| | | | | | \n|─| |─| |─| \n|   |───|   \n|───|       \n|           '
 
     assert str(fusion) == expected
 
 
 @pytest.mark.fusion
-def test_apply_ising_fusion(state):
-    fusion = Fusion(state)
+def test_apply_ising_fusion(ising_state):
+    fusion = Fusion(ising_state)
 
     psi = [1, 0, 0]
     vacuum = [0, 1, 0]
@@ -40,47 +47,87 @@ def test_apply_ising_fusion(state):
     assert fusion.apply_fusion(psi_sigma, psi_sigma) == [1, 2, 2]
     assert not fusion.apply_fusion(psi_sigma, psi_sigma) == [2, 1, 2]  # get owned rishi
 
+@pytest.mark.fusion
+def test_apply_fibo_fusion(fibo_state):
+    fusion = Fusion(fibo_state)
+
+    tau = [1,0]
+    vac = [0,1]
+
+    assert fusion.apply_fusion(tau, tau) == [1,1]
+    assert fusion.apply_fusion(vac,vac) == vac
+    assert fusion.apply_fusion(tau, vac) == tau
+
+    tau_vac = [1,1]
+
+    assert fusion.apply_fusion(tau_vac,  tau_vac) == [3,2]
 
 @pytest.mark.fusion
-def test_qubit_enc(state):
-    state.add_operation(1, FusionPair(0, 1))
-    state.add_operation(1, FusionPair(2, 3))
-    state.add_operation(1, FusionPair(4, 5))
-    state.add_operation(2, FusionPair(2, 4))
-    state.add_operation(3, FusionPair(0, 2))
+def test_ising_qubit_enc(ising_state):
+    ising_state.add_operation(1, FusionPair(0, 1))
+    ising_state.add_operation(1, FusionPair(2, 3))
+    ising_state.add_operation(1, FusionPair(4, 5))
+    ising_state.add_operation(2, FusionPair(2, 4))
+    ising_state.add_operation(3, FusionPair(0, 2))
 
-    fusion = Fusion(state)
+    fusion = Fusion(ising_state)
     correct = [FusionPair(0, 1), FusionPair(2, 4), FusionPair(2, 3)]
 
     assert set(map(str, fusion.qubit_enc())) == set(map(str, correct))
 
+@pytest.mark.fusion
+def test_fibo_qubit_enc(fibo_state):
+
+    fusion =  Fusion(fibo_state)
+
+    pass
+
+
 
 @pytest.mark.fusion
-def test_verify_fusion_result(state):
-    fusion = Fusion(state)
+def test_ising_verify_fusion_result(ising_state):
+    fusion = Fusion(ising_state)
     assert not fusion.verify_fusion_result(TopoCharge.from_ising(IsingTopoCharge.Sigma))
     assert fusion.verify_fusion_result(TopoCharge.from_ising(IsingTopoCharge.Vacuum))
     assert fusion.verify_fusion_result(TopoCharge.from_ising(IsingTopoCharge.Psi))
 
-    state.add_anyon(Anyon('7', TopoCharge.from_ising(IsingTopoCharge.Sigma), (0, 0)))
-    fusion = Fusion(state)
+    ising_state.add_anyon(Anyon('7', TopoCharge.from_ising(IsingTopoCharge.Sigma), (0, 0)))
+    fusion = Fusion(ising_state)
     assert not fusion.verify_fusion_result(TopoCharge.from_ising(IsingTopoCharge.Vacuum))
     assert not fusion.verify_fusion_result(TopoCharge.from_ising(IsingTopoCharge.Psi))
     assert fusion.verify_fusion_result(TopoCharge.from_ising(IsingTopoCharge.Sigma))
 
 @pytest.mark.fusion
-def test_minimum_possible_anyons(state):
+def test_fibo_verify_fusion_result(fibo_state):
+    fusion = Fusion(fibo_state)
 
+    assert fusion.verify_fusion_result(TopoCharge.from_fibonacci(FibonacciTopoCharge.Tau))
+    assert fusion.verify_fusion_result(TopoCharge.from_fibonacci(FibonacciTopoCharge.Vacuum))
+    
+    state = State()
+    state.set_anyon_model(AnyonModel.Fibonacci)
+    for i in range(6):
+        state.add_anyon(Anyon(f'{i}', TopoCharge.from_fibonacci(FibonacciTopoCharge.Vacuum), (0,0)))
     fusion = Fusion(state)
+
+    assert not fusion.verify_fusion_result(TopoCharge.from_fibonacci(FibonacciTopoCharge.Tau))
+    assert fusion.verify_fusion_result(TopoCharge.from_fibonacci(FibonacciTopoCharge.Vacuum))
+
+
+    
+
+@pytest.mark.fusion
+def test_ising_minimum_possible_anyons(ising_state):
+
+    fusion = Fusion(ising_state)
 
     assert (fusion.minimum_possible_anyons(10) == [21,22])
     assert (fusion.minimum_possible_anyons(5) == [11,12])
 
-    state.set_anyon_model(AnyonModel.Fibonacci)
+@pytest.mark.fusion
+def test_fibo_minimum_possible_anyons(ising_state):
+    ising_state.set_anyon_model(AnyonModel.Fibonacci)
+    fusion = Fusion(ising_state)
 
-    fusion = Fusion(state)
-
-    
-
-    assert(fusion.minimum_possible_anyons(10) == [17,18]);
-    assert(fusion.minimum_possible_anyons(0) == [0,1,2,3]);
+    assert(fusion.minimum_possible_anyons(10) == [17,18])
+    assert(fusion.minimum_possible_anyons(0) == [0,1,2,3])
--- src/fusion/fusion.rs
+++ src/fusion/fusion.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 
 use crate::fusion::state::State;
+use crate::model::anyon::FibonacciTopoCharge;
 use crate::model::anyon::IsingTopoCharge;
 use crate::model::anyon::TopoCharge;
 use crate::model::model::AnyonModel;
@@ -58,37 +59,48 @@ pub struct Fusion {
 impl Fusion {
     /// Converts from IsingTopoCharge to internal format
     /// Format is [psi, vacuum, sigma]  (so we can use the index as the encode)
-    pub fn ising_canonical_topo_charge(&self, charge: IsingTopoCharge) -> [u64; 3] {
+    pub fn ising_canonical_topo_charge(&self, charge: IsingTopoCharge) -> Vec<u64> {
         match charge {
-            IsingTopoCharge::Psi => [1, 0, 0],
-            IsingTopoCharge::Vacuum => [0, 1, 0],
-            IsingTopoCharge::Sigma => [0, 0, 1],
+            IsingTopoCharge::Psi => vec![1, 0, 0],
+            IsingTopoCharge::Vacuum => vec![0, 1, 0],
+            IsingTopoCharge::Sigma => vec![0, 0, 1],
         }
     }
 
-    /// Creates a qubit encoding from the fusion tree. The encoding is a list of
+    /// Converts from FibonacciTopoCharge to internal format
+    /// Format is [tau, vacuum]  (so we can use the index as the encode)
+    pub fn fibonacci_canonical_topo_charge(&self, charge: FibonacciTopoCharge) -> Vec<u64> {
+        match charge {
+            FibonacciTopoCharge::Tau => vec![1, 0],
+            FibonacciTopoCharge::Vacuum => vec![0, 1],
+        }
+    }
+
+    /// Creates a qubit encoding for the Ising model from the fusion tree. The encoding is a list of
     /// FusionPairs that represent the anyons that are fused to create the qubit
     /// encoding.
     pub fn ising_qubit_enc(&self) -> Vec<FusionPair> {
-        let mut tcs: Vec<[u64; 3]> = self
+        let mut tcs: Vec<Vec<u64>> = self
             .state
             .anyons()
             .iter()
             .map(|a| self.ising_canonical_topo_charge(a.charge().get_ising()))
             .collect();
-        let mut fusion_pair_tc: HashMap<FusionPair, [u64; 3]> = HashMap::new();
+        let mut fusion_pair_tc: HashMap<FusionPair, Vec<u64>> = HashMap::new();
 
-        let mut final_tc: [u64; 3] = [0, 0, 0];
+        let mut final_tc: Vec<u64> = vec![0, 0, 0];
 
         for (i, op) in self.ops.iter().enumerate() {
             for (j, fusion_pair) in op.iter().enumerate() {
-                let tc =
-                    self.ising_apply_fusion(tcs[fusion_pair.anyon_1()], tcs[fusion_pair.anyon_2()]);
+                let tc = self.ising_apply_fusion(
+                    tcs[fusion_pair.anyon_1()].clone(),
+                    tcs[fusion_pair.anyon_2()].clone(),
+                );
                 if i == self.ops.len() - 1 && j == op.len() - 1 {
                     final_tc = tc;
                     break;
                 }
-                fusion_pair_tc.insert(fusion_pair.clone(), tc);
+                fusion_pair_tc.insert(fusion_pair.clone(), tc.clone());
                 tcs[fusion_pair.anyon_1()] = tc;
             }
         }
@@ -112,8 +124,17 @@ impl Fusion {
         encoding_fusions
     }
 
+    /// Creates a qubit encoding for the Fibonacci model from the fusion tree. The encoding is a list of
+    /// FusionPairs that represent the anyons that are fused to create the qubit
+    /// encoding.
+    pub fn fibonacci_qubit_enc(&self) -> Vec<FusionPair> {
+        unimplemented!()
+    }
+
     /// Applies the fusion rules to two anyons and returns the resulting anyon(s).
-    pub fn ising_apply_fusion(&self, anyon_1: [u64; 3], anyon_2: [u64; 3]) -> [u64; 3] {
+    pub fn ising_apply_fusion(&self, anyon_1: Vec<u64>, anyon_2: Vec<u64>) -> Vec<u64> {
+        assert!(anyon_1.len() == 3 && anyon_2.len() == 3);
+
         let add = |a: [u64; 3], b: [u64; 3]| -> [u64; 3] { std::array::from_fn(|i| a[i] + b[i]) };
         let arr_scale = |a: [u64; 3], b: u64| -> [u64; 3] { std::array::from_fn(|i| a[i] * b) };
 
@@ -142,15 +163,45 @@ impl Fusion {
             }
         }
 
-        output
+        Vec::from(output)
+    }
+    /// Applies the fusion rules to two anyones and returns the resulting anyon(s).
+    fn fibonacci_apply_fusion(&self, anyon_1: Vec<u64>, anyon_2: Vec<u64>) -> Vec<u64> {
+        assert!(anyon_1.len() == 2 && anyon_2.len() == 2);
+
+        let add = |a: [u64; 2], b: [u64; 2]| -> [u64; 2] { std::array::from_fn(|i| a[i] + b[i]) };
+        let arr_scale = |a: [u64; 2], b: u64| -> [u64; 2] { std::array::from_fn(|i| a[i] * b) };
+
+        let mut output = [0 as u64; 2];
+
+        // ising fusion rules
+        // symmetric matrix which is built from fusion rules of (tau, 1) ^ (tau, 1)
+        let fusion_rules_mtx: [[[u64; 2]; 2]; 2] = [[[1, 1], [1, 0]], [[1, 0], [0, 1]]];
+
+        // build the outer product of the two tc vectors
+        let mut tc_mtx = [[0; 2]; 2];
+        for i in 0..2 {
+            for j in 0..2 {
+                tc_mtx[i][j] = anyon_1[i] * anyon_2[j];
+            }
+        }
+
+        // mtx multiply fusion rules with tc_mtx
+        for i in 0..2 {
+            for j in 0..2 {
+                output = add(output, arr_scale(fusion_rules_mtx[i][j], tc_mtx[i][j]));
+            }
+        }
+
+        Vec::from(output)
     }
 
     /// Checks if an overall fusion result is possible given the state's
-    /// configuration and an initial topo charge
+    /// configuration and an initial topo charge under the Ising model
     ///
     /// Precondition: Non empty list of anyons
     pub fn ising_verify_fusion_result(&self, init_charge: IsingTopoCharge) -> bool {
-        let overall_fusion_result: [u64; 3] = self
+        let overall_fusion_result: Vec<u64> = self
             .state
             .anyons()
             .iter()
@@ -166,43 +217,61 @@ impl Fusion {
             .all(|(a, b)| *b <= 0 || *a > 0)
     }
 
+    /// Checks if an overall fusion result is possible given the state's
+    /// configuration and an initial topo charge under the Fibonacci model
+    ///
+    /// Precondition: Non empty list of anyons
+    pub fn fibonacci_verify_fusion_result(&self, init_charge: FibonacciTopoCharge) -> bool {
+        let overall_fusion_result: Vec<u64> = self
+            .state
+            .anyons()
+            .iter()
+            .map(|a| self.fibonacci_canonical_topo_charge(a.charge().get_fibonacci()))
+            .reduce(|acc, tc| self.fibonacci_apply_fusion(acc, tc))
+            .unwrap();
+
+        // if an element > 0 that means it was our initial charge, so we need to
+        // check if our final fusion result also has that element > 0
+        overall_fusion_result
+            .iter()
+            .zip(self.fibonacci_canonical_topo_charge(init_charge).iter())
+            .all(|(a, b)| *b <= 0 || *a > 0)
+    }
+
     ///
     /// Returns number of sigmas that can be in the initial topological charges of anyons to exactly a certain number of qubits for the Ising model
-    /// 
-    pub fn ising_possible_sigmas(&self, qubits:u32) -> Vec<u32>{
-        vec![2*qubits+1, 2*qubits+2]
+    ///
+    pub fn ising_possible_sigmas(&self, qubits: u32) -> Vec<u32> {
+        vec![2 * qubits + 1, 2 * qubits + 2]
     }
     ///
     /// Returns number of taus that can be in the initial topological charges of anyons to exactly a certain number of qubits for the Fibonacci model
-    /// 
+    ///
     /// Precondition: Requires qubits <=30
-    pub fn fibonacci_possible_taus(&self, qubits:u32) -> Vec<u32> {
-
-        if qubits ==0{
-            return vec![0,1,2,3];
+    pub fn fibonacci_possible_taus(&self, qubits: u32) -> Vec<u32> {
+        if qubits == 0 {
+            return vec![0, 1, 2, 3];
         }
 
         let mut possible_taus = Vec::new();
 
         let mut n = 1;
 
-
         // We have that the fibonacci recurrence gives us that tau^n = a + b * tau
         let mut a = 0;
         let mut b = 1;
-        
+
         // Checks that b < 2^(qubits+1) meaning as otherwise fusing to tau would result in too many qubits
-        while b < 1 << (qubits+1) {
-            
+        while b < 1 << (qubits + 1) {
             // If b >= 2^qubits we have that this will give exactly 'qubits' qubits
-            if (1 << (qubits)) <= b{
+            if (1 << (qubits)) <= b {
                 possible_taus.push(n);
             }
 
-            b=a+b;
-            a=b-a;
+            b = a + b;
+            a = b - a;
 
-            n+=1;
+            n += 1;
         }
         // Accounts for the case that we can also fuse to vaccuum
         possible_taus.push(n);
@@ -240,6 +309,7 @@ impl Fusion {
     fn qubit_enc(&self) -> PyResult<Vec<FusionPair>> {
         match self.state.anyon_model() {
             AnyonModel::Ising => Ok(self.ising_qubit_enc()),
+            AnyonModel::Fibonacci => Ok(self.fibonacci_qubit_enc()),
             _ => Err(PyValueError::new_err("This model is not supported yet")),
         }
     }
@@ -293,13 +363,10 @@ impl Fusion {
         Ok(format!("{}\n{}\n{}{}", top_level, level_2, body, last_time).to_string())
     }
 
-    fn apply_fusion(
-        &self,
-        anyon_1: [u64; 3],
-        anyon_2: [u64; 3],
-    ) -> PyResult<[u64; 3]> {
+    fn apply_fusion(&self, anyon_1: Vec<u64>, anyon_2: Vec<u64>) -> PyResult<Vec<u64>> {
         match self.state.anyon_model() {
             AnyonModel::Ising => Ok(self.ising_apply_fusion(anyon_1, anyon_2)),
+            AnyonModel::Fibonacci => Ok(self.fibonacci_apply_fusion(anyon_1, anyon_2)),
             _ => Err(PyValueError::new_err("This model is not supported yet")),
         }
     }
@@ -307,15 +374,17 @@ impl Fusion {
     fn verify_fusion_result(&self, init_charge: TopoCharge) -> bool {
         match self.state.anyon_model() {
             AnyonModel::Ising => self.ising_verify_fusion_result(init_charge.get_ising()),
+            AnyonModel::Fibonacci => {
+                self.fibonacci_verify_fusion_result(init_charge.get_fibonacci())
+            }
             _ => false,
         }
     }
-    fn minimum_possible_anyons(&self, qubits: u32) -> PyResult<Vec<u32>>{
+    fn minimum_possible_anyons(&self, qubits: u32) -> PyResult<Vec<u32>> {
         match self.state.anyon_model() {
             AnyonModel::Ising => Ok(self.ising_possible_sigmas(qubits)),
             AnyonModel::Fibonacci => Ok(self.fibonacci_possible_taus(qubits)),
-            _ => Err(PyValueError::new_err("This model is not supported yet"))
+            _ => Err(PyValueError::new_err("This model is not supported yet")),
         }
-
     }
 }
